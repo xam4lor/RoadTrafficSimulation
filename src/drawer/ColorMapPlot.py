39b5feca96fffa169e1efb919e7cdae0c6ee9fb0
xam4lor <xam4lor@gmail.com>
2023-02-18 18:58:01 +0100

    Plot now takes less longer to draw

--- src/drawer/ColorMapPlot.py
+++ src/drawer/ColorMapPlot.py
@@ -8,23 +8,31 @@ class ColorMapPlot(PlotType):
     """
     def draw(self, config, uValues):
         # Size of dimensions
-        Nt = int(len(uValues)/2)
-        Nx = len(uValues[0])
-        x = np.linspace(0, config["config"]["x_max"], Nx)
-        t = np.linspace(0, config["config"]["t_max"], Nt)
+        xPoints = 200
+        tPoints = 200
+        x = np.linspace(0, config["config"]["x_max"], xPoints)
+        t = np.linspace(0, config["config"]["t_max"], tPoints)
+
+        # Find corresponding indices
+        xIndices = np.zeros(xPoints)
+        tIndices = np.zeros(tPoints)
+        for i in range(0, xPoints):
+            xIndices[i] = int(i * len(uValues[0]) / xPoints)
+        for i in range(0, tPoints):
+            tIndices[i] = int(i * len(uValues) / tPoints + 2)
 
         # Create an array for the u values
-        u = np.zeros((Nt, Nx))
-        for i in range(0, Nt):
-            for j in range(0, Nx):
-                u[i, j] = uValues[i][j]
+        u = np.zeros([tPoints, xPoints])
+        for i in range(0, tPoints):
+            for j in range(0, xPoints):
+                u[i, j] = uValues[int(tIndices[i]), int(xIndices[j])]
 
         # Plot the u values as a color map
         plt.figure()
         plt.pcolor(x, t, u, cmap='coolwarm')
         plt.colorbar()
-        plt.xlabel('x')
-        plt.ylabel('t')
+        plt.xlabel('Distance from origin $x$ (m)')
+        plt.ylabel('Time $t$ (s)')
         plt.title('Density of cars $\\rho(x,t)$')
 
         # Save the plot
