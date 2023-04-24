aebb16ef4ef0bb7fef7d98ae4cba13c3dc8ae90d
FrostWolfyy <chammouma.marwan@gmail.com>
2023-04-24 14:37:58 +0200

    Create 2nd order Scheme Class (Not finished)

---
+++ src/integration/SecondOrderGodunov.py
@@ -0,0 +1,28 @@
+from src.integration.NumericalScheme import NumericalScheme
+import numpy as np
+
+## Il manque uRight et Alpha à rajouter dans la classe
+class SecondOrderGodunov(NumericalScheme):
+    """
+    Second order Godunov scheme.
+
+    Parameters
+    ----------
+    config : dict
+        The configuration dictionary.
+    """
+    def __init__(self, config, selectNumericalScheme):
+        # Parameters
+        self.vm = config["schemes"]["second_order_godunov"]["vm"]
+        self.rhom = config["schemes"]["second_order_godunov"]["rhom"]
+        self.a = config["schemes"]["second_order_godunov"]["a"]
+        self.alpha = config["schemes"]["second_order_godunov"]["alpha"]
+
+        self.dx = config["config"]["dx"]
+        self.dt = config["config"]["dt"]
+
+    ### Numerical schemes ###
+    def u(self, ui, uLefti, x, t):
+        v = self.vm
+        return ui - v * (1.0 - 2.0 * ui / self.rhom) * self.dt / self.dx * (ui - uLefti) + alpha * self.dt / self.dx**2 * (uRighti - 2*ui + uLefti)
+    
